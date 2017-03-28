788d1e7149330e77f3d0af891da6ceb740473400
Daniel <daniel.vernberg@gmail.com>
2017-03-28 18:50:07 +0200

    JSDoc for service

--- src/app/hipster-ipsum.service.ts
+++ src/app/hipster-ipsum.service.ts
@@ -5,16 +5,24 @@ import { Http, Response } from '@angular/http';
 @Injectable()
 export class HipsterIpsumService {
 
+  /**
+   * Creates an instance of HipsterIpsumService.
+   * @param {Http} http 
+   * 
+   * @memberOf HipsterIpsumService
+   */
   constructor(private http: Http) { }
 
-  /*
-    Make http get request to API endpoint
-    @param {string} type - The text type to be generated
-    @param {int} paragraphs - The nr of paragraphs to be generated
-
-    @return response of json format
-  */
-  getIpsum(type, paragraphs) {
+  /**
+   * Make HTTP request to API
+   * 
+   * @param {string} type 
+   * @param {number} paragraphs 
+   * @returns http get request
+   * 
+   * @memberOf HipsterIpsumService
+   */
+  getIpsum(type: string, paragraphs: number) {
     return this.http.get(`http://hipsterjesus.com/api/?paras=${paragraphs}&type=${type}&html=true`)
     .map((res:Response) => res.json());
   }
